792b7cc870b2639bad4eafee2ed0113f12abbe47
Ananya Kaushik <ananya@aserto.com>
2023-06-09 13:41:25 -0700

    use python sdk for reader

--- directory.py
+++ directory.py
@@ -6,6 +6,8 @@ import grpc
 
 from google.protobuf.json_format import MessageToDict
 
+from aserto.client import Directory
+
 from aserto.directory.common.v2 import ObjectIdentifier, RelationIdentifier, RelationTypeIdentifier
 from aserto.directory.reader.v2 import GetObjectRequest, GetObjectResponse, GetRelationRequest, ReaderStub
 
@@ -17,37 +19,32 @@ cert = os.path.expandvars(os.getenv("DIRECTORY_GRPC_CERT_PATH", ""))
 api_key = os.getenv("ASERTO_DIRECTORY_API_KEY")
 tenant_id = os.getenv("ASERTO_TENANT_ID")
 
+config = {"api_key": api_key, "tenant_id": tenant_id, "address": address, "cert": cert}
+ds = Directory(config)
+
 
 def user_from_identity(sub) -> Dict[str, Any]:
-    with grpc.secure_channel(target=address, credentials=_channel_credentials()) as channel:
-        reader = ReaderStub(channel)
+    reader = ds.reader
 
-        relationResp = reader.GetRelation(
-            GetRelationRequest(
-                param=RelationIdentifier(
-                    subject=ObjectIdentifier(type="user"),
-                    relation=RelationTypeIdentifier(name="identifier", object_type="identity"),
-                    object=ObjectIdentifier(type="identity", key=sub),
+    relationResp = ds.reader.GetRelation(
+        GetRelationRequest(
+            param=RelationIdentifier(
+                subject=ObjectIdentifier(type="user"),
+                relation=RelationTypeIdentifier(
+                    name="identifier", object_type="identity"
                 ),
+                object=ObjectIdentifier(type="identity", key=sub),
             ),
-            metadata=_metadata(api_key, tenant_id)
-        )
+        ),
+        metadata=_metadata(api_key, tenant_id),
+    )
 
-        return _get_object(reader, relationResp.results[0].subject)
+    return _get_object(reader, relationResp.results[0].subject)
 
 def user_from_key(key) -> Dict[str, Any]:
-    with grpc.secure_channel(target=address, credentials=_channel_credentials()) as channel:
-        reader = ReaderStub(channel)
+    reader = ds.reader
 
-        return _get_object(reader, ObjectIdentifier(type="user", key=key))
-
-def _channel_credentials() -> grpc.ChannelCredentials:
-    if cert:
-        with open(cert, "rb") as f:
-            return grpc.ssl_channel_credentials(f.read())
-    else:
-        return grpc.ssl_channel_credentials()
-
+    return _get_object(reader, ObjectIdentifier(type="user", key=key))
 
 
 def _metadata(api_key: Optional[str], tenant_id: Optional[str]) -> Tuple:
