48148b5efa20549683b5fa264a9d7f083f2eb73b
Ananya Kaushik <ananya@aserto.com>
2023-06-12 14:31:21 -0700

    add fix

--- directory.py
+++ directory.py
@@ -19,12 +19,11 @@ cert = os.path.expandvars(os.getenv("DIRECTORY_GRPC_CERT_PATH", ""))
 api_key = os.getenv("ASERTO_DIRECTORY_API_KEY")
 tenant_id = os.getenv("ASERTO_TENANT_ID")
 
-config = {"api_key": api_key, "tenant_id": tenant_id, "address": address, "cert": cert}
-ds = Directory(config)
+config = {"api_key": api_key, "tenant_id": tenant_id, "address": address, "ca_cert": cert}
+ds = Directory(**config)
 
 
 def user_from_identity(sub) -> Dict[str, Any]:
-    reader = ds.reader
 
     relationResp = ds.reader.GetRelation(
         GetRelationRequest(
@@ -39,12 +38,10 @@ def user_from_identity(sub) -> Dict[str, Any]:
         metadata=_metadata(api_key, tenant_id),
     )
 
-    return _get_object(reader, relationResp.results[0].subject)
+    return _get_object(ds.reader, relationResp.results[0].subject)
 
 def user_from_key(key) -> Dict[str, Any]:
-    reader = ds.reader
-
-    return _get_object(reader, ObjectIdentifier(type="user", key=key))
+    return _get_object(ds.reader, ObjectIdentifier(type="user", key=key))
 
 
 def _metadata(api_key: Optional[str], tenant_id: Optional[str]) -> Tuple:
